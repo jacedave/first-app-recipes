1c8834bf25041c6af579084d083693f84a07b976
Jason Loosle <jason.loosle@aggiemail.usu.edu>
2021-02-02 10:26:54 -0700

    Adding some stuff.

--- app/src/main/java/com/example/projectone/MainActivity.kt
+++ app/src/main/java/com/example/projectone/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.projectone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var submitButton: Button
+    private lateinit var editText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var myText = R.string.instructions_main
+
+        submitButton = findViewById(R.id.submit_button)
+        editText = findViewById(R.id.editTextIngredient)
+
+        editText.setOnClickListener {
+            editText.setText("")
+        }
+
+        submitButton.setOnClickListener {
+            val messageResId = when {
+                myText == R.string.instructions_main -> R.string.instruction_Toast
+                else -> R.string.instruction_Toast
+            }
+
+            Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+                    .show()
+        }
     }
 }
